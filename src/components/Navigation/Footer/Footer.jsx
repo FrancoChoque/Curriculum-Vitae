0bfdd2b648e110dc2a9ff96b767c15dffd903805
FrancoChoque <franco.choquescipione@gmail.com>
2019-05-10 00:57:38 -0300

    add footer

--- src/components/Navigation/Footer/Footer.jsx
+++ src/components/Navigation/Footer/Footer.jsx
@@ -7,10 +7,29 @@ import styles from './Footer.module.css';
 const Footer = () => (
   <footer className={styles.Footer}>
     <p>About me</p>
-    <div>
-      <FontAwesomeIcon icon={faLinkedin} />
-      <FontAwesomeIcon icon={faGithub} />
-      <FontAwesomeIcon icon={faEnvelope} />
+    <div className={styles.FooterCol}>
+      <div>
+        <FontAwesomeIcon icon={faLinkedin} />
+        <span className={styles.DesktopOnly}>
+        View my Linkedin profile
+        </span>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faGithub} />
+        <span className={styles.DesktopOnly}>
+        Check me out on
+          <span>
+            {' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/FrancoChoque">@FrancoChoque</a>
+          </span>
+        </span>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faEnvelope} />
+        <span className={styles.DesktopOnly}>
+        franco.choquescipione@gmail.com
+        </span>
+      </div>
     </div>
   </footer>
 );
