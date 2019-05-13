e9fefd1ea154b3021c20c9060fdedcd37b35b184
Franco Choque <franco.choquescipione@gmail.com>
2019-05-13 19:39:01 -0300

    add cv

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
+import styles from './CV.module.css';
 
 const CV = () => (
-  <div>
-    CVCVCV
+  <div className={styles.Container}>
+    <div className={styles.Layout}>
+      <div>
+        Franco Choque
+      </div>
+      <div>
+      Franco Choque
+      </div>
+    </div>
   </div>
 );
 
